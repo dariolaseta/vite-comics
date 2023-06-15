8c5116ff4d8d7b90ad6eec5e80e6eeb997748766
dariolaseta <dario.laseta@gmail.com>
2023-06-15 14:49:16 +0200

    created and imported AppHeader, AppMain & AppFooter

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default{
+  name: "App",
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter
+  } 
+  }
 </script>
 
 <template>
